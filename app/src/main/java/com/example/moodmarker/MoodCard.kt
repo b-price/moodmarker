135b2ba85ff92ddee9ee6155b450ba5d90a8f791
ashley <amerin2@students.towson.edu>
2024-04-10 16:46:28 -0400

    Added a Submit Button and Date Text to MoodCard

--- app/src/main/java/com/example/moodmarker/MoodCard.kt
+++ app/src/main/java/com/example/moodmarker/MoodCard.kt
@@ -1,5 +1,6 @@
 package com.example.moodmarker
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -7,13 +8,16 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.em
@@ -30,7 +34,19 @@ fun MoodCard(nav: NavHostController) {
             .fillMaxWidth()
     ) {
         Column {
-            Spacer(modifier = Modifier.height(150.dp))
+            Spacer(modifier = Modifier.height(100.dp))
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Text("Date: ")
+                //Add code so user can enter the date
+
+                Button(onClick = { } ) {
+                    Text("Submit")
+                }
+            }
+
         }
         Column(
             modifier = Modifier.fillMaxWidth()
