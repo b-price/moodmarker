88c861e622e354c25d8d6fcd3a83b7e9799371d0
b-price <bprice12@students.towson.edu>
2024-05-07 16:55:17 -0400

    Made moodcard look prettier

--- app/src/main/java/com/example/moodmarker/MoodCard.kt
+++ app/src/main/java/com/example/moodmarker/MoodCard.kt
@@ -1,5 +1,8 @@
 package com.example.moodmarker
 
+import android.util.Log
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -23,6 +26,7 @@ import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.em
@@ -46,68 +50,70 @@ fun MoodCard(
             .fillMaxWidth()
     ) {
         Column {
-            Spacer(modifier = Modifier.height(100.dp))
+
+
+
+        }
+        Column(
+            modifier = Modifier.fillMaxWidth()
+        ) {
             Row(
                 modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.SpaceBetween
-            ) {
-                Button(onClick = {
-                    if (isEdit()){
-                        vm.updateMoodMarker(moodMarker.value)
-                        onEdit()
-                        vm.setPresetMoodMarker(MoodMarker(0, EmotionType.Happy, "", false))
-                    } else {
-                        vm.addMoodMarker(moodMarker.value)
-                    }
-                    nav.popBackStack()
-                    })
-                     {
-                    Text("Submit")
+                horizontalArrangement = Arrangement.SpaceEvenly,
+                verticalAlignment = Alignment.CenterVertically
+            ){
+                if (moodMarker.value.emotionType == EmotionType.Angry) {
+                    Text("\uD83D\uDE21", fontSize = 18.em)
+                } else if (moodMarker.value.emotionType == EmotionType.Sad) {
+                    Text("\uD83D\uDE41", fontSize = 18.em)
+                } else if (moodMarker.value.emotionType == EmotionType.Neutral) {
+                    Text("\uD83D\uDE10", fontSize = 18.em)
+                } else if (moodMarker.value.emotionType == EmotionType.Happy) {
+                    Text("\uD83D\uDE42", fontSize = 18.em)
+                } else {
+                    Text("\uD83D\uDE01", fontSize = 18.em)
                 }
                 if (moodMarker.value.isFavorite){
                     Icon(
                         Icons.Default.Favorite,
                         "Favorite",
-                        modifier = Modifier.clickable { moodMarker.value = moodMarker.value.copy(isFavorite = false) }
-                            .size(30.dp),
+                        modifier = Modifier
+                            .clickable {
+                                moodMarker.value = moodMarker.value.copy(isFavorite = false)
+                            }
+                            .size(40.dp),
                         tint = MaterialTheme.colorScheme.primary)
                 } else {
                     Icon(
                         Icons.Outlined.FavoriteBorder,
                         "Not Favorite",
-                        modifier = Modifier.clickable { moodMarker.value = moodMarker.value.copy(isFavorite = true) }
-                            .size(30.dp),
+                        modifier = Modifier
+                            .clickable {
+                                moodMarker.value = moodMarker.value.copy(isFavorite = true)
+                            }
+                            .size(40.dp),
                         tint = MaterialTheme.colorScheme.primary)
                 }
             }
 
         }
         Column(
-            modifier = Modifier.fillMaxWidth()
-        ) {
-            if (moodMarker.value.emotionType == EmotionType.Angry) {
-                Text("\uD83D\uDE21", fontSize = 18.em)
-            } else if (moodMarker.value.emotionType == EmotionType.Sad) {
-                Text("\uD83D\uDE41", fontSize = 18.em)
-            } else if (moodMarker.value.emotionType == EmotionType.Neutral) {
-                Text("\uD83D\uDE10", fontSize = 18.em)
-            } else if (moodMarker.value.emotionType == EmotionType.Happy) {
-                Text("\uD83D\uDE42", fontSize = 18.em)
-            } else {
-                Text("\uD83D\uDE01", fontSize = 18.em)
-            }
-        }
-        Column(
             modifier = Modifier.padding(16.dp)
         ){
-            Row() {
+            Row(
+                horizontalArrangement = Arrangement.Center,
+                modifier = Modifier.fillMaxWidth()
+            ) {
                 Text("\uD83D\uDE21", fontSize = 8.em)
                 Text("\uD83D\uDE41", fontSize = 8.em)
                 Text("\uD83D\uDE10", fontSize = 8.em)
                 Text("\uD83D\uDE42", fontSize = 8.em)
                 Text("\uD83D\uDE01", fontSize = 8.em)
             }
-            Row() {
+            Row(
+                horizontalArrangement = Arrangement.Center,
+                modifier = Modifier.fillMaxWidth()
+            ) {
                 RadioButton(selected = moodMarker.value.emotionType == EmotionType.Angry,
                     onClick = {
                         moodMarker.value = moodMarker.value.copy(emotionType = EmotionType.Angry)
@@ -134,16 +140,64 @@ fun MoodCard(
                     }
                 )
             }
-            TextField(
-                value = moodMarker.value.dailyEntry,
-                onValueChange = {
-                    moodMarker.value = moodMarker.value.copy(dailyEntry = it)
-                },
-                label = { Text("Enter your MoodMarker") }
-            )
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceEvenly
+            ) {
+                TextField(
+                    value = moodMarker.value.dailyEntry,
+                    onValueChange = {
+                        moodMarker.value = moodMarker.value.copy(dailyEntry = it)
+                    },
+                    label = { Text("Enter your MoodMarker") }
+                )
+            }
+            Spacer(modifier = Modifier.height(30.dp))
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceEvenly
+            ) {
+                Button(onClick = {
+                    if (isEdit()){
+                        vm.updateMoodMarker(moodMarker.value)
+                        onEdit()
+                        vm.setPresetMoodMarker(MoodMarker(0, EmotionType.Happy, "", false))
+                    } else {
+                        vm.addMoodMarker(moodMarker.value)
+                    }
+                    nav.popBackStack()
+                })
+                {
+                    Text("Submit", fontSize = 7.em, modifier = Modifier.padding(4.dp))
+                }
+            }
         }
     }
 }
+
+//fun getImage(){
+//    // Registers a photo picker activity launcher in single-select mode.
+//    val pickMedia = registerForActivityResult(ActivityResultContracts.PickVisualMedia()) { uri ->
+//        // Callback is invoked after the user selects a media item or closes the
+//        // photo picker.
+//        if (uri != null) {
+//            Log.d("PhotoPicker", "Selected URI: $uri")
+//        } else {
+//            Log.d("PhotoPicker", "No media selected")
+//        }
+//    }
+//    // Launch the photo picker and let the user choose only images.
+//    pickMedia.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+//}
+//
+//fun registerForActivityResult(
+//    pickVisualMedia: ActivityResultContracts.PickVisualMedia,
+//    any: Any
+//): Any {
+//    TODO("Not yet implemented")
+//}
+
+
 enum class EmotionType {
     Angry, Sad, Neutral, Happy, Excited
 }
--- app/src/main/java/com/example/moodmarker/MoodMarkerRepository.kt
+++ app/src/main/java/com/example/moodmarker/MoodMarkerRepository.kt
@@ -4,18 +4,11 @@ import android.app.Application
 import androidx.room.Room
 
 class MoodMarkerRepository(app: Application) : IMoodMarkerRepository {
-    //private var _entries = listOf<MoodMarker>()
     private val db: MoodMarkersDatabase
-    private val sample = " Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam pulvinar tortor erat, nec dignissim arcu tincidunt ac. Morbi volutpat facilisis tortor, aliquet egestas enim. In non leo sapien. Nunc molestie mauris sit amet congue iaculis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Maecenas ut egestas odio. Nam a dui eu neque faucibus congue luctus nec arcu."
-
-    //Makes 10 sample entries
     init {
         db = Room.databaseBuilder(app, MoodMarkersDatabase::class.java, "moodmarkers.db")
             .fallbackToDestructiveMigration()
             .build()
-//        _entries = (0..10).map { i ->
-//            MoodMarker(i, EmotionType.entries.random(), i.toString() + sample, true, Date())
-//        }
     }
 
     override suspend fun getMoodMarkers(): List<MoodMarker> {
