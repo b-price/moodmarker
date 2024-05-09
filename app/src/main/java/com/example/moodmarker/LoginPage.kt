2663941bbbae3825d0c1825f1cebd52d59c12d3c
Craig Neely <cneely6@students.towson.edu>
2024-05-09 15:44:40 -0400

    Updated Login Layout

--- app/src/main/java/com/example/moodmarker/LoginPage.kt
+++ app/src/main/java/com/example/moodmarker/LoginPage.kt
@@ -1,100 +1,150 @@
 package com.example.moodmarker
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.Button
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.em
 import androidx.navigation.NavHostController
+import com.example.moodmarker.ui.theme.MoodMarkerTheme
+import com.example.moodmarker.ui.theme.components.LoginFields
 
 @Composable
 fun LoginPage(nav: NavHostController) {
     //TODO: Implement login w/credentials
-    val userName = remember { mutableStateOf("") }
-    val password = remember { mutableStateOf("") }
+    val (userName, setUserName) = rememberSaveable { mutableStateOf("") }
+    val (password, setPassword) = rememberSaveable { mutableStateOf("") }
+    val (rememberMe, setRememberMe) = rememberSaveable { mutableStateOf(false) }
 
 //    val emojiList = listOf("😁", "😡", "🙁", "🙂", "😐")
 //    val randomEmojiOne = emojiList.shuffled().take(1)[0]
 //    val randomEmojiTwo = emojiList.shuffled().take(1)[0]
 
     Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Row {
-            Spacer(modifier = Modifier.height(25.dp))
-        }
-
-//        Row(
-//            verticalAlignment = Alignment.CenterVertically,
-//        ){
-//            Text(text = "M", fontSize = 18.em)
-//            Text(text = randomEmojiOne, fontSize = 10.em)
-//            Text(text = randomEmojiTwo, fontSize = 10.em)
-//            Text(text = "d", fontSize = 18.em)
-//        }
-//        Row(
-//            verticalAlignment = Alignment.CenterVertically,
-//        ){
-//            Text(text = "Marker", fontSize = 18.em)
-//        }
-
-        Row {
-            Spacer(modifier = Modifier.height(50.dp))
-        }
+        
+        /** Login Header Text **/
+        Text(
+            text = "Login",
+            style = MaterialTheme.typography.displayMedium,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier
+                .padding(vertical = 16.dp)
+                .align(alignment = Alignment.Start)
+            //color = Color.
+        )
 
 
-        Row(modifier = Modifier
-            .fillMaxWidth()
-            .padding(start = 4.dp, end = 4.dp),
+        /** Username Login Field **/
+        LoginFields(
+            value = userName,
+            onValueChange = setUserName,
+            labelText = "Username",
+            leadingIcon = Icons.Default.Person,
+            modifier = Modifier.fillMaxWidth()
+        )
+        Spacer(modifier = Modifier.height(10.dp))
+
+
+        /** Password Login Field **/
+        LoginFields(
+            value = password,
+            onValueChange = setPassword,
+            labelText = "Password",
+            leadingIcon = Icons.Default.Lock,
+            modifier = Modifier.fillMaxWidth(),
+            visualTransformation = PasswordVisualTransformation(),
+            keyboardType = KeyboardType.Password
+        )
+        Spacer(modifier = Modifier.height(10.dp))
+
+
+        /** Remember Me Checkbox **/
+        Row (
+            horizontalArrangement = Arrangement.Center,
             verticalAlignment = Alignment.CenterVertically
-        ){
-            Text(text = "UserName", modifier = Modifier.width(100.dp))
-            TextField(
-                value = userName.value,
-                onValueChange = { userName.value = it }
-            )
+        ) {
+            Checkbox(checked = rememberMe, onCheckedChange = setRememberMe)
+            Text("Remember Me")
+        }
+        Spacer(modifier = Modifier.height(10.dp))
+
+
+//        /** Login and Signup Buttons **/
+//        Row (modifier = Modifier.padding(50.dp), verticalAlignment = Alignment.CenterVertically){
+//            Button(onClick = { nav.navigate(Routes.MainPage.route) }, modifier = Modifier.padding(10.dp) ) {
+//                Text("Login", fontSize = 5.em, modifier = Modifier.padding(5.dp))
+//            }
+//            Button(onClick = { nav.navigate(Routes.CreateAccount.route) }) {
+//                Text("Signup", fontSize = 5.em, modifier = Modifier.padding(5.dp))
+//            }
+//        }
+
+        /** Login Button **/
+        Button(onClick = { nav.navigate(Routes.MainPage.route) }, modifier = Modifier.fillMaxWidth() ) {
+                Text("Login", fontSize = 5.em)
+        }
+        Spacer(modifier = Modifier.height(10.dp))
+
+
+        /** Forgot Password Button **/
+        TextButton(onClick = {}) {
+            Text("Forgot Password?")
+        }
+        Spacer(modifier = Modifier.height(10.dp))
+
+
+        /** Signup Button **/
+        Row(horizontalArrangement = Arrangement.SpaceEvenly,
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .fillMaxSize()
+                .wrapContentSize(align = Alignment.BottomCenter)
+            ) {
+            Text("Don't have an account?")
+            Spacer(Modifier.height(8.dp))
+            TextButton(onClick = { nav.navigate(Routes.CreateAccount.route) }) {
+                Text("Sign Up")
+            }
         }
 
-        Row(modifier = Modifier
-            .fillMaxWidth()
-            .padding(start = 4.dp, end = 4.dp),
-            verticalAlignment = Alignment.CenterVertically
-        ){
-            Text(text = "Password", modifier = Modifier.width(100.dp))
-            TextField(
-                value = password.value,
-                onValueChange = { password.value = it }
-            )
-        }
 
-        Row (modifier = Modifier.padding(50.dp), verticalAlignment = Alignment.CenterVertically){
-            Button(onClick = { nav.navigate(Routes.MainPage.route) }, modifier = Modifier.padding(10.dp) ) {
-                Text("Login", fontSize = 5.em, modifier = Modifier.padding(5.dp))
-            }
-            Button(onClick = { nav.navigate(Routes.CreateAccount.route) }) {
-                Text("Signup", fontSize = 5.em, modifier = Modifier.padding(5.dp))
-            }
-        }
+
+
 
 
     }
 
-
-
-
-    Row {
-        Spacer(modifier = Modifier.height(100.dp))
-    }
 }
+
---
+++ app/src/main/java/com/example/moodmarker/ui/theme/components/LoginFields.kt
@@ -0,0 +1,39 @@
+package com.example.moodmarker.ui.theme.components
+
+import android.graphics.drawable.Icon
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+
+
+@Composable
+fun LoginFields(
+    modifier: Modifier = Modifier,
+    value:String,
+    onValueChange:(String) -> Unit,
+    labelText:String,
+    leadingIcon: ImageVector? = null,
+    keyboardType: KeyboardType = KeyboardType.Text,
+    visualTransformation: VisualTransformation = VisualTransformation.None
+
+){
+    OutlinedTextField(
+        modifier = modifier,
+        value = value,
+        onValueChange = onValueChange,
+        label = { Text(labelText) },
+        keyboardOptions = KeyboardOptions(keyboardType = keyboardType),
+        visualTransformation = visualTransformation,
+        shape = RoundedCornerShape(50   ),
+        leadingIcon = {if (leadingIcon != null) Icon(imageVector = leadingIcon, null) }
+    )
+}
